102a5b73c02720967bda65e78f79ee109e554689
rajashree-guha <rajashree.625.guha@gmail.com>
2023-08-25 18:53:38 +0530

    Project_Groove

---
+++ app.js
@@ -0,0 +1,8 @@
+var buybtn = document.getElementById("buy-now")
+
+var text =document.getElementById("thanks-text")
+
+buybtn.onclick =()=>{
+    text.style.display="block"
+    buybtn.style.display="none"
+}
